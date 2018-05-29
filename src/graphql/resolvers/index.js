f6cf83c7e50756a55550a779dca9ce18f030b0a2
manojkmishra <manojmishra571980@gmail.com>
2018-05-29 14:10:18 +1000

    user returned back in graphql after crate tweet

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,15 +1,17 @@
 import GraphQLDate from 'graphql-date';
 import TweetResolvers from './tweet-resolvers';
 import UserResolvers from './user-resolvers';
+import User from '../../models/User';
 
 export default 
-{  Date: GraphQLDate,
+{ Date: GraphQLDate,
+  Tweet: { user: ({ user }) => User.findById(user), },
   Query: 
   {   getTweet: TweetResolvers.getTweet,
       getTweets: TweetResolvers.getTweets,
       me: UserResolvers.me
   },
- Mutation: 
+  Mutation: 
    { createTweet: TweetResolvers.createTweet,
      updateTweet: TweetResolvers.updateTweet,
      deleteTweet: TweetResolvers.deleteTweet,
